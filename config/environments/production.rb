982350099d8ea7e3bc0788817bd4413eee165dac
Taylor Griffin <tygriffin@gmail.com>
2013-05-26 13:10:38 +0900

    heroku take 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ SEZWebsite::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( article.css )
+  config.assets.precompile += %w( article.css, popup_base.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
