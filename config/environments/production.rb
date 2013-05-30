06e7d690ac5ca67599503ec8ae0a55948ff47ea2
Taylor Griffin <tygriffin@gmail.com>
2013-05-31 02:12:45 +0900

    fixed precompile error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ SEZWebsite::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += ['article.css', 'popup_base.css', 'home.css', 'archive.css', 'blog.css', 'application_mobile.css', 'article_mobile', 'application_mobile.js']
+  config.assets.precompile += ['article.css', 'popup_base.css', 'home.css', 'archive.css', 'blog.css', 'application_mobile.css', 'article_mobile.css', 'application_mobile.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
