f96b75ce1178c4d1d8bb361de1397da2bbdb8997
Taylor Griffin <tygriffin@gmail.com>
2013-06-02 19:29:12 +0900

    fixing IE

--- app/assets/javascripts/popup_bubble.js
+++ app/assets/javascripts/popup_bubble.js
@@ -15,82 +15,73 @@ $(document).ready(function() {
 
 
   // Home page popups ////////////////////////////////
-  $(function() {
-    $('.bubble_wrapper').each(function() {
-      $('.bubble_trigger').click(function(e) {
-        e.stopPropagation();
-        $('.bubble_info').hide();
-        $(this).next().stop().show('fast', function() {
-          alert( $('.bubble_info').css("display"));
-        });
-        if($.browser.msie) {  // fix for IE
-          $('.bubble_info').css({"visibility":"visible"});
-        }
-        $('.bubble_info').css({"visibility":"visible"});
-        alert( $('.bubble_info').css("display"));
-      });
 
-      $('.bubble_close').click(function(e) {
-        e.stopPropagation();
-        $('.bubble_info').hide('fast');
-      });
-    });
-    $('html').click(function() {
-      $('.bubble_info').hide('fast');
-    });
+  $('.bubble_trigger').click(function(e) {
+    e.stopPropagation();
+    $('.bubble_info').hide();
+    $(this).next().stop().show('fast');
+    alert( 'hello?');
   });
 
-  // Article body popups /////////////////////////////
-  $(function() {
-    var hide_delay = 0;
-    var hide_delay_timer = null;
+  $('.bubble_close').click(function(e) {
+    e.stopPropagation();
+    $('.bubble_info').hide('fast');
+  });
 
-    $('.article_bubble_trigger').mouseenter(function(e) {
-      if (hide_delay_timer) clearTimeout(hide_delay_timer);
-      $('.popup_placeholder').stop();
-      //Vocabulary word popup
-      for (var i=0; i<gon.vocab.length; i++) {
-        var entry = gon.vocab[i];
-        if ($(this).attr('id') == entry['word']) {
-          $(this).append('<p class="article_bubble">'+entry['translation']+'</p>');
-        }
+  $('html').click(function() {
+    $('.bubble_info').hide('fast');
+  });
+
+
+  // Article body popups /////////////////////////////
+  var hide_delay = 0;
+  var hide_delay_timer = null;
+
+  $('.article_bubble_trigger').mouseenter(function(e) {
+    if (hide_delay_timer) clearTimeout(hide_delay_timer);
+    $('.popup_placeholder').stop();
+    //Vocabulary word popup
+    for (var i=0; i<gon.vocab.length; i++) {
+      var entry = gon.vocab[i];
+      if ($(this).attr('id') == entry['word']) {
+        $(this).append('<p class="article_bubble">'+entry['translation']+'</p>');
       }
-      //Culture note popup
-      for (var i=0; i<gon.culture_notes.length; i++) {
-        var entry = gon.culture_notes[i];
-        if ($(this).attr('id') == entry['title']) {
-          $(this).append('<p class="article_bubble">'+entry['body']+'</p>');
-        }
+    }
+    //Culture note popup
+    for (var i=0; i<gon.culture_notes.length; i++) {
+      var entry = gon.culture_notes[i];
+      if ($(this).attr('id') == entry['title']) {
+        $(this).append('<p class="article_bubble">'+entry['body']+'</p>');
       }
-      $('.article_bubble').css({
-        position: 'fixed',
-        bottom: '30px',
-        left: '10px',
-        fontSize: '130%',
-        padding: '5px',
-        margin: '0',
-        maxWidth: '600px',
-        opacity: '0.95',
-        backgroundColor: '#6f8500',
-        color: '#fff',
-        zIndex: '200'
-      });
-      $('.article_bubble').hide();
-      $('.article_bubble').show('fast');
-      $('.popup_placeholder').hide();
-      $('.article_bubble').mouseover(function() {
-        if (hide_delay_timer) clearTimeout(hide_delay_timer);
-      })
-    }).mouseout(function() {
-      if (hide_delay_timer) clearTimeout(hide_delay_timer);
-      hide_delay_timer = setTimeout(function() {
-        hide_delay_timer = null;
-        $('.article_bubble').hide('fast', function() {
-          $(this).closest('.article_bubble').remove();
-        });
-        $('.popup_placeholder').show('fast');
-      }, hide_delay);
+    }
+    $('.article_bubble').css({
+      position: 'fixed',
+      bottom: '30px',
+      left: '10px',
+      fontSize: '130%',
+      padding: '5px',
+      margin: '0',
+      maxWidth: '600px',
+      opacity: '0.95',
+      backgroundColor: '#6f8500',
+      color: '#fff',
+      zIndex: '200'
     });
+    $('.article_bubble').hide();
+    $('.article_bubble').show('fast');
+    $('.popup_placeholder').hide();
+    $('.article_bubble').mouseover(function() {
+      if (hide_delay_timer) clearTimeout(hide_delay_timer);
+    })
+  }).mouseout(function() {
+    if (hide_delay_timer) clearTimeout(hide_delay_timer);
+    hide_delay_timer = setTimeout(function() {
+      hide_delay_timer = null;
+      $('.article_bubble').hide('fast', function() {
+        $(this).closest('.article_bubble').remove();
+      });
+      $('.popup_placeholder').show('fast');
+    }, hide_delay);
   });
 
 });
