c7092ee3c515befaada12503102dcca54893666d
Taylor Griffin <tygriffin@gmail.com>
2013-06-13 02:06:55 +0900

    fixed undefined method .empty error

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,12 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "mail.simpleenglishzine.com",
+  :address              => "mail.tayloryuengriffin.com",
   :port                 => 26,
-  :domain               => "simpleenglishzine.com",
-  :user_name            => "info@simpleenglishzine.com",
+  :domain               => "tayloryuengriffin.com",
+  :user_name            => "info@tayloryuengriffin.com",
   :password             => "xVYiKVj3XzmK66Vy",
   :authentication       => :plain,
   :enable_starttls_auto => true,
   :openssl_verify_mode  => OpenSSL::SSL::VERIFY_NONE
 }
 
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+ActionMailer::Base.default_url_options[:host] = "simpleenglishzine.com"
