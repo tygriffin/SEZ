46946e63f8068b1bc6c97534556a42ee1358255f
Taylor Griffin <tygriffin@gmail.com>
2013-08-28 16:14:19 +0900

    fixed topic controller

--- app/controllers/topic_controller.rb
+++ app/controllers/topic_controller.rb
@@ -1,10 +1,6 @@
 class TopicController < ApplicationController
   def show
-    if mobile_device?
-      @local_stylesheet = "topic_mobile.css"
-    else
-      @local_stylesheet = "topic.css"
-    end
+    @local_stylesheet = "topic.css"
     @topic = Topic.find(params[:id])
     @title = @topic.name
     @articles = Article.where(:topic_id => @topic.id).order("pubdate DESC").limit(20)
